b7b1f4503f4fff1ba4d3ec62a1f1573fff78995d
Adrian Mirabal <adrian.mirabal07@gmail.com>
2025-04-15 17:22:21 +0000

    if the pagination have several pages make that the client can go to a page directly as a option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 interface Monster {
     id: string;
@@ -35,361 +36,361 @@ const initialMonsters: Monster[] = [
     {
         id: "1",
         name: "Goblin",
-        photo: "https://picsum.photos/200/300",
+        photo: "/goblin.png",
         description: "A small, mischievous creature."
     },
     {
         id: "2",
         name: "Dragon",
-        photo: "https://picsum.photos/200/300",
+        photo: "/dragon.png",
         description: "A large, fire-breathing reptile."
     },
   {
     id: "3",
     name: "Ogre",
-    photo: "https://picsum.photos/200/300",
+    photo: "/ogre.png",
     description: "A big and ugly creature."
   },
   {
     id: "4",
     name: "Troll",
-    photo: "https://picsum.photos/200/300",
+    photo: "/troll.png",
     description: "A very big and ugly creature."
   },
   {
     id: "5",
     name: "Sprite",
-    photo: "https://picsum.photos/200/300",
+    photo: "/sprite.png",
     description: "A small fairy."
   },
   {
     id: "6",
     name: "Gorgon",
-    photo: "https://picsum.photos/200/300",
+    photo: "/gorgon.png",
     description: "A snake-haired monster."
   },
   {
     id: "7",
     name: "Centaur",
-    photo: "https://picsum.photos/200/300",
+    photo: "/centaur.png",
     description: "A half-man, half-horse creature."
   },
   {
     id: "8",
     name: "Harpy",
-    photo: "https://picsum.photos/200/300",
+    photo: "/harpy.png",
     description: "A bird-woman creature."
   },
   {
     id: "9",
     name: "Phoenix",
-    photo: "https://picsum.photos/200/300",
+    photo: "/phoenix.png",
     description: "A fire bird creature."
   },
   {
     id: "10",
     name: "Basilisk",
-    photo: "https://picsum.photos/200/300",
+    photo: "/basilisk.png",
     description: "A snake creature."
   },
   {
     id: "11",
     name: "Hydra",
-    photo: "https://picsum.photos/200/300",
+    photo: "/hydra.png",
     description: "A multi-headed serpent."
   },
   {
     id: "12",
     name: "Cerberus",
-    photo: "https://picsum.photos/200/300",
+    photo: "/cerberus.png",
     description: "A multi-headed dog."
   },
     {
         id: "13",
         name: "Minotaur",
-        photo: "https://picsum.photos/200/300",
+        photo: "/minotaur.png",
         description: "A creature with the head of a bull and the body of a man."
     },
     {
         id: "14",
         name: "Siren",
-        photo: "https://picsum.photos/200/300",
+        photo: "/siren.png",
         description: "A beautiful but dangerous creature that lures sailors with its enchanting voice."
     },
     {
         id: "15",
         name: "Cyclops",
-        photo: "https://picsum.photos/200/300",
+        photo: "/cyclops.png",
         description: "A giant with a single eye in the middle of its forehead."
     },
     {
         id: "16",
         name: "Chimera",
-        photo: "https://picsum.photos/200/300",
+        photo: "/chimera.png",
         description: "A fire-breathing monster with the head of a lion, a goat's body, and a serpent's tail."
     },
     {
         id: "17",
         name: "Griffin",
-        photo: "https://picsum.photos/200/300",
+        photo: "/griffin.png",
         description: "A legendary creature with the body, tail, and back legs of a lion; the head and wings of an eagle; and an eagle's talons as its front feet."
     },
     {
         id: "18",
         name: "Hippogriff",
-        photo: "https://picsum.photos/200/300",
+        photo: "/hippogriff.png",
         description: "A legendary creature with the front half of an eagle and the hind half of a horse."
     },
     {
         id: "19",
         name: "Kraken",
-        photo: "https://picsum.photos/200/300",
+        photo: "/kraken.png",
         description: "A legendary sea monster of giant size and cephalopod appearance."
     },
     {
         id: "20",
         name: "Leviathan",
-        photo: "https://picsum.photos/200/300",
+        photo: "/leviathan.png",
         description: "A sea monster referenced in the Bible."
     },
     {
         id: "21",
         name: "Roc",
-        photo: "https://picsum.photos/200/300",
+        photo: "/roc.png",
         description: "A legendary enormous bird of prey."
     },
     {
         id: "22",
         name: "Thunderbird",
-        photo: "https://picsum.photos/200/300",
+        photo: "/thunderbird.png",
         description: "A legendary creature in certain North American indigenous cultures' history and culture. It is considered a supernatural being of power and strength."
     },
     {
       id: "23",
       name: "Grendel",
-      photo: "https://picsum.photos/200/300",
+      photo: "/grendel.png",
       description: "A monstrous creature from the epic poem Beowulf."
   },
   {
       id: "24",
       name: "Medusa",
-      photo: "https://picsum.photos/200/300",
+      photo: "/medusa.png",
       description: "A Gorgon with snakes for hair whose gaze could turn men to stone."
   },
   {
       id: "25",
       name: "Banshee",
-      photo: "https://picsum.photos/200/300",
+      photo: "/banshee.png",
       description: "A female spirit in Irish folklore whose wails foretell death."
   },
   {
       id: "26",
       name: "Yeti",
-      photo: "https://picsum.photos/200/300",
+      photo: "/yeti.png",
       description: "A large, ape-like creature said to inhabit the Himalayan region."
   },
   {
       id: "27",
       name: "Vampire",
-      photo: "https://picsum.photos/200/300",
+      photo: "/vampire.png",
       description: "A mythical being who subsists by feeding on the blood of the living."
   },
   {
       id: "28",
       name: "Werewolf",
-      photo: "https://picsum.photos/200/300",
+      photo: "/werewolf.png",
       description: "A person who transforms into a wolf-like creature during a full moon."
   },
   {
       id: "29",
       name: "Zombie",
-      photo: "https://picsum.photos/200/300",
+      photo: "/zombie.png",
       description: "A reanimated corpse, often depicted as feeding on human flesh."
   },
   {
       id: "30",
       name: "Mummy",
-      photo: "https://picsum.photos/200/300",
+      photo: "/mummy.png",
       description: "A deceased human or animal whose body has been preserved."
   },
   {
       id: "31",
       name: "Ghoul",
-      photo: "https://picsum.photos/200/300",
+      photo: "/ghoul.png",
       description: "A monstrous humanoid creature that feeds on corpses."
   },
   {
       id: "32",
       name: "Imp",
-      photo: "https://picsum.photos/200/300",
+      photo: "/imp.png",
       description: "A small, mischievous demon or fairy."
   },
   {
     id: "33",
     name: "Salamander",
-    photo: "https://picsum.photos/200/300",
+    photo: "/salamander.png",
     description: "A mythical creature resembling a lizard, believed to live in fire."
   },
   {
     id: "34",
     name: "Gargoyle",
-    photo: "https://picsum.photos/200/300",
+    photo: "/gargoyle.png",
     description: "A grotesque carved human or animal face or figure projecting from the gutter of a building."
   },
   {
     id: "35",
     name: "Will-o'-the-Wisp",
-    photo: "https://picsum.photos/200/300",
+    photo: "/will-o-the-wisp.png",
     description: "A mysterious light seen by travelers at night, especially over bogs."
   },
   {
     id: "36",
     name: "Baba Yaga",
-    photo: "https://picsum.photos/200/300",
+    photo: "/baba-yaga.png",
     description: "A supernatural being (or one of a trio of sisters with the same name) who appears as a deformed and/or ferocious-looking old woman in Slavic folklore."
   },
   {
     id: "37",
     name: "Leshy",
-    photo: "https://picsum.photos/200/300",
+    photo: "/leshy.png",
     description: "A tutelary deity of the forests in Slavic mythology."
   },
   {
     id: "38",
     name: "Domovoy",
-    photo: "https://picsum.photos/200/300",
+    photo: "/domovoy.png",
     description: "A household spirit in Slavic folklore."
   },
   {
     id: "39",
     name: "Vodyanoy",
-    photo: "https://picsum.photos/200/300",
+    photo: "/vodyanoy.png",
     description: "A male water spirit in Slavic mythology."
   },
   {
     id: "40",
     name: "Rusalka",
-    photo: "https://picsum.photos/200/300",
+    photo: "/rusalka.png",
     description: "A female water spirit in Slavic mythology."
   },
     {
         id: "41",
         name: "Dryad",
-        photo: "https://picsum.photos/200/300",
+        photo: "/dryad.png",
         description: "A tree nymph or tree spirit in Greek mythology."
     },
     {
         id: "42",
         name: "Satyr",
-        photo: "https://picsum.photos/200/300",
+        photo: "/satyr.png",
         description: "A creature that is part man and part goat."
     },
     {
         id: "43",
         name: "Sphinx",
-        photo: "https://picsum.photos/200/300",
+        photo: "/sphinx.png",
         description: "A mythical creature with the head of a human and the body of a lion."
     },
     {
         id: "44",
         name: "Bicorn",
-        photo: "https://picsum.photos/200/300",
+        photo: "/bicorn.png",
         description: "A mythical creature similar to a unicorn, but with two horns."
     },
     {
         id: "45",
         name: "Questing Beast",
-        photo: "https://picsum.photos/200/300",
+        photo: "/questing-beast.png",
         description: "A monster from Arthurian legend with the head and neck of a serpent, the body of a leopard, the haunches of a lion, and the feet of a hart."
     },
     {
         id: "46",
         name: "Kelpie",
-        photo: "https://picsum.photos/200/300",
+        photo: "/kelpie.png",
         description: "A shape-shifting water spirit inhabiting the lochs and pools of Scotland."
     },
     {
         id: "47",
         name: "Manticore",
-        photo: "https://picsum.photos/200/300",
+        photo: "/manticore.png",
         description: "A Persian legendary creature similar to the Egyptian sphinx. It has the body of a red lion, a human head with three rows of sharp teeth, and a trumpet-like voice."
     },
     {
         id: "48",
         name: "Pegasus",
-        photo: "https://picsum.photos/200/300",
+        photo: "/pegasus.png",
         description: "One of the best known creatures in Greek mythology. He is a winged divine stallion usually depicted as pure white."
     },
     {
         id: "49",
         name: "Wyvern",
-        photo: "https://picsum.photos/200/300",
+        photo: "/wyvern.png",
         description: "A dragon-like creature with two legs and a barbed tail."
     },
     {
         id: "50",
         name: "Amarok",
-        photo: "https://picsum.photos/200/300",
+        photo: "/amarok.png",
         description: "A giant wolf in Inuit mythology, said to hunt anyone foolish enough to venture out alone at night."
     },
     {
         id: "51",
         name: "Unicorn",
-        photo: "https://picsum.photos/200/300",
+        photo: "/unicorn.png",
         description: "A mythical creature usually depicted as a white horse with a single, pointed horn projecting from its forehead."
     },
     {
         id: "52",
         name: "Nymph",
-        photo: "https://picsum.photos/200/300",
+        photo: "/nymph.png",
         description: "A mythological spirit of nature imagined as a beautiful maiden inhabiting rivers, woods, or other locations."
     },
     {
         id: "53",
         name: "Salamander",
-        photo: "https://picsum.photos/200/300",
+        photo: "/salamander.png",
         description: "A mythical creature often depicted as a lizard-like animal that lives in fire or has an affinity with fire."
     },
     {
         id: "54",
         name: "Baku",
-        photo: "https://picsum.photos/200/300",
+        photo: "/baku.png",
         description: "A Japanese supernatural being that devours nightmares."
     },
     {
         id: "55",
         name: "Grootslang",
-        photo: "https://picsum.photos/200/300",
+        photo: "/grootslang.png",
         description: "A legendary creature from South African folklore, said to be a primordial being with the features of an elephant and a serpent."
     },
     {
         id: "56",
         name: "Wendigo",
-        photo: "https://picsum.photos/200/300",
+        photo: "/wendigo.png",
         description: "A malevolent, cannibalistic spirit or creature from the folklore of the First Nations Algonquin people."
     },
     {
         id: "57",
         name: "Taniwha",
-        photo: "https://picsum.photos/200/300",
+        photo: "/taniwha.png",
         description: "A supernatural being in Māori culture, often taking the form of a mythical sea creature or reptile."
     },
     {
         id: "58",
         name: "Ahuitzotl",
-        photo: "https://picsum.photos/200/300",
+        photo: "/ahuitzotl.png",
         description: "A mythical creature in Aztec mythology, described as a dog-like creature with human-like hands, a monkey's body, and a long tail ending in a hand."
     },
     {
         id: "59",
         name: "Yacumama",
-        photo: "https://picsum.photos/200/300",
+        photo: "/yacumama.png",
         description: "A legendary giant serpent said to inhabit the Amazon River basin."
     },
     {
         id: "60",
         name: "Tarasque",
-        photo: "https://picsum.photos/200/300",
+        photo: "/tarasque.png",
         description: "A fearsome legendary dragon-like creature from French folklore, said to have terrorized the region of Tarascon."
     }
 ];
@@ -608,7 +609,22 @@ export default function Home() {
                         <ChevronLeft className="mr-2 h-4 w-4" />
                         Previous
                     </Button>
-                    <span>{currentPage} / {totalPages}</span>
+                     {totalPages > 5 ? (
+                        <Select value={String(currentPage)} onValueChange={(value) => handlePageChange(Number(value))}>
+                            <SelectTrigger className="w-[120px]">
+                                <SelectValue placeholder="Select page..." />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                                    <SelectItem key={page} value={String(page)}>
+                                        {page}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    ) : (
+                        <span>{currentPage} / {totalPages}</span>
+                    )}
                     <Button
                         size="sm"
                         onClick={() => handlePageChange(currentPage + 1)}
